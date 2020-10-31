2cf81970c4c38036b2251a1b7a140ca2cd1198e4
Alix Lahuec <alix.lahuec@mail.mcgill.ca>
2020-10-31 15:43:05 -0400

    Changed default title to bibkey

--- defaults/preferences/defaults.js
+++ defaults/preferences/defaults.js
@@ -1,4 +1,4 @@
 pref("extensions.roamexport.recursive_collection_topics", false);
-pref("extensions.roamexport.citekey_as_title", false);
+pref("extensions.roamexport.citekey_as_title", true);
 pref("extensions.roamexport.indent_tag_as_child", "<blockquote>");
 pref("extensions.roamexport.convert_underline_to_heading", true);
